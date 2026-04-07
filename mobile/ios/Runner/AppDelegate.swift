83ddabc4eb528a95d3af0b36c17a22ce948d7691
casa <alvarez.rodrigo194@gmail.com>
2026-04-06 22:46:27 -0400

    Feat: Actualizacion sin api key

--- mobile/ios/Runner/AppDelegate.swift
+++ mobile/ios/Runner/AppDelegate.swift
@@ -8,7 +8,8 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyAAEE2DRbffugL1fzq9chIZ9GYGtcqPShY")
+    let mapsKey = Bundle.main.object(forInfoDictionaryKey: "MAPS_API_KEY") as? String ?? ""
+    GMSServices.provideAPIKey(mapsKey)
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
